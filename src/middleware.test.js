94250b94efc93e241632d81624f18195b00475e7
Joe Fleming <joe.fleming@gmail.com>
2017-04-26 18:22:20 -0700

    add more middleware tests

--- src/middleware.test.js
+++ src/middleware.test.js
@@ -11,19 +11,19 @@ test.beforeEach(() => {
   nextHandler = middleware({ dispatch: doDispatch, getState: doGetState });
 });
 
-test('nextHandler must return a function to handle next', (t) => {
+test('nextHandler returns a function to handle next', (t) => {
   t.is(typeof nextHandler, 'function');
   t.is(nextHandler.length, 1);
 });
 
-test('actionHandler must return a function to handle action', (t) => {
+test('actionHandler returns a function to handle action', (t) => {
   const actionHandler = nextHandler();
   t.is(typeof actionHandler, 'function');
   t.is(actionHandler.length, 1);
 });
 
 // non-FSA checks
-test('actionHandler must pass action to next if not a promise', (t) => {
+test('actionHandler passes action to next if not a promise', (t) => {
   t.plan(1);
   const actionObj = {};
 
@@ -31,7 +31,7 @@ test('actionHandler must pass action to next if not a promise', (t) => {
   actionHandler(actionObj);
 });
 
-test('actionHandler must return value as expected if a promise', (t) => {
+test('actionHandler returns value as expected if a promise', (t) => {
   t.plan(1);
   const expected = 'rocks';
   const actionHandler = nextHandler();
@@ -42,7 +42,7 @@ test('actionHandler must return value as expected if a promise', (t) => {
 });
 
 // FSA checks
-test('actionHandler must pass FSA action to next if not a promise', (t) => {
+test('actionHandler passes FSA action to next if not a promise', (t) => {
   t.plan(1);
   const fsaActionObj = {
     type: 'test',
@@ -55,7 +55,7 @@ test('actionHandler must pass FSA action to next if not a promise', (t) => {
   actionHandler(fsaActionObj);
 });
 
-test('actionHandler must return value as expected if FSA payload promise', (t) => {
+test('actionHandler returns value as expected if FSA payload promise', (t) => {
   t.plan(1);
   const payload = 'rocks';
   const expected = {
@@ -66,7 +66,6 @@ test('actionHandler must return value as expected if FSA payload promise', (t) =
       testing: true,
     },
   };
-  const actionHandler = nextHandler();
   const fsaActionObj = {
     type: 'test',
     payload: Promise.resolve(payload),
@@ -75,5 +74,48 @@ test('actionHandler must return value as expected if FSA payload promise', (t) =
     },
   };
 
+  const actionHandler = nextHandler();
   return actionHandler(fsaActionObj).then(action => t.deepEqual(action, expected));
+});
+
+test('actionHandler dispatches request & ok actions in order if resolved', (t) => {
+  t.plan(2);
+  let callIndex = 0;
+  const actionTypes = ['test_REQUEST', 'test_OK'];
+
+  doDispatch = (action) => {
+    t.is(action.type, actionTypes[callIndex]);
+    callIndex += 1;
+  };
+  nextHandler = middleware({ dispatch: doDispatch, getState: doGetState });
+
+  const fsaActionObj = {
+    type: 'test',
+    payload: Promise.resolve('ok'),
+  };
+
+  const actionHandler = nextHandler();
+  return actionHandler(fsaActionObj);
+});
+
+
+test('actionHandler dispatches request & error actions in order if rejected', (t) => {
+  t.plan(2);
+  let callIndex = 0;
+  const actionTypes = ['test_REQUEST', 'test_ERROR'];
+
+  doDispatch = (action) => {
+    t.is(action.type, actionTypes[callIndex]);
+    callIndex += 1;
+  };
+  nextHandler = middleware({ dispatch: doDispatch, getState: doGetState });
+
+  const fsaActionObj = {
+    type: 'test',
+    payload: Promise.reject('nope'),
+  };
+
+  const actionHandler = nextHandler();
+  return actionHandler(fsaActionObj)
+  .catch(() => 'silence'); //silence the rejected promise
 });
