6b5fbe4febdce7d1f17929a27b0de6f96441c495
Joe Fleming <joe.fleming@gmail.com>
2017-04-28 12:30:38 -0700

    add promise state helpers

--- src/helpers.js
+++ src/helpers.js
@@ -1,5 +1,21 @@
 const { request, ok, error } = require('./actionTypes');
 
+// action type name helpers
 exports.onRequest = actionType => `${actionType}_${request}`;
 exports.onSuccess = actionType => `${actionType}_${ok}`;
 exports.onError = actionType => `${actionType}_${error}`;
+
+// action promise status helpers
+const getMetaValue = (action, name) => {
+  if (!action.meta) throw new Error('Action does not have a meta property');
+  return action.meta[name];
+};
+exports.isLoading = (action) => {
+  const loading = getMetaValue(action, 'loading');
+  if (typeof loading === 'undefined') {
+    throw new Error('Action meta does not contain a loading property');
+  }
+  return loading;
+};
+exports.isResolved = action => !exports.isLoading(action) && !Boolean(action.error);
+exports.isRejected = action => !exports.isLoading(action) && Boolean(action.error);
