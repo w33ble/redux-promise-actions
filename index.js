ef3b0e92f3fad6b559646f89594eb498da27472e
Joe Fleming <joe.fleming@gmail.com>
2017-04-28 11:35:16 -0700

    expose actiontype name helpers

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const middleware = require('./src/middleware');
 const handlePromiseAction = require('./src/handlePromiseAction');
+const { onRequest, onSuccess, onError } = require('./src/helpers');
 
 module.exports = {
   middleware,
   handlePromiseAction,
+  onRequest,
+  onSuccess,
+  onError,
 };
