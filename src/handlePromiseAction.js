6fcbe866db2644b8c343135affd6727e3a91cc88
Joe Fleming <joe.fleming@gmail.com>
2017-04-28 11:26:03 -0700

    use actiontype helpers in reducer helper

--- src/handlePromiseAction.js
+++ src/handlePromiseAction.js
@@ -1,11 +1,12 @@
 const { handleActions } = require('redux-actions');
 const { identity } = require('@w33bletools/simpleutils');
-const actionTypes = require('./actionTypes');
+const { onRequest, onSuccess, onError } = require('./helpers');
 
 module.exports = function handlePromiseAction(type, reducer = identity, defaultState) {
-  const actionValues = Object.keys(actionTypes).map(key => actionTypes[key]);
-  const handlers = actionValues.reduce((acc, actionType) => Object.assign(acc, {
-    [`${type}_${actionType}`]: reducer,
-  }), {});
+  const handlers = {
+    [onRequest(type)]: reducer,
+    [onSuccess(type)]: reducer,
+    [onError(type)]: reducer,
+  };
   return handleActions(handlers, defaultState);
 };
